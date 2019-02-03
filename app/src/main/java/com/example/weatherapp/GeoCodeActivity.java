9de758cdb582a8ce51747672f91039786b146285
Blake Gardner <blakeagardner@utexas.edu>
2019-02-03 10:44:51 -0600

    JSON finished

--- app/src/main/java/com/example/weatherapp/GeoCodeActivity.java
+++ app/src/main/java/com/example/weatherapp/GeoCodeActivity.java
@@ -5,8 +5,19 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class GeoCodeActivity extends AppCompatActivity {
+
+    Button btnShowCoord;
+    EditText edtAddress;
+    TextView txtCoord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +30,46 @@ public class GeoCodeActivity extends AppCompatActivity {
         ProgressDialog dialog = new ProgressDialog(GeoCodeActivity.this);
 
         @Override
-        protected void onPreExecute(){}
+        protected void onPreExecute(){
+            super.onPreExecute();
+            dialog.setMessage("Please wait...");
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
 
         @Override
         protected String doInBackground(String... strings) {
+            String response;
+            try{
+                String address  = strings[0];
+                HttpDataHandler http = new HttpDataHandler();
+                String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?address=%s", address);
+                response = http.getHTTPData(url);
+                return response;
+            } catch(Exception ex){
+
+            }
             return null;
         }
 
         @Override
-        protected void onPostExecute(String s){}
+        protected void onPostExecute(String s){
+           try{
+               JSONObject jsonObject = new JSONObject(s);
+
+               String lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+                       .getJSONObject("location").get("lat").toString();
+               String lng = ((JSONArray)jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+                       .getJSONObject("location").get("lng").toString();
+
+               txtCoord.setText(String.format("Coordinates : %s / %s ",lat,lng));
+
+               if(dialog.isShowing())
+                   dialog.dismiss();
+
+           } catch(JSONException e){
+               e.printStackTrace();
+           }
+        }
     }
 }
