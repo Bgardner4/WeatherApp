26403c13757c7f064467093757b0a372006a276a
rabiakhan713 <rabiakhan@utexas.edu>
2019-02-03 14:07:53 -0600

    Added GeoCodeActivity functions

--- app/src/main/java/com/example/weatherapp/GeoCodeActivity.java
+++ app/src/main/java/com/example/weatherapp/GeoCodeActivity.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,17 +23,28 @@ public class GeoCodeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_geo_code);
+        setContentView(R.layout.activity_main);
+
+        btnShowCoord = (Button)findViewById(R.id.btnShowCoordinates);
+        edtAddress = (EditText)findViewById(R.id.edtAddress);
+        txtCoord = (TextView)findViewById(R.id.txtCoordinates);
+
+        btnShowCoord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new GetCoordinates().execute(edtAddress.getText().toString().replace(" ","+"));
+            }
+        });
+
     }
 
     private class GetCoordinates extends AsyncTask<String,Void,String> {
-
         ProgressDialog dialog = new ProgressDialog(GeoCodeActivity.this);
 
         @Override
-        protected void onPreExecute(){
+        protected void onPreExecute() {
             super.onPreExecute();
-            dialog.setMessage("Please wait...");
+            dialog.setMessage("Please wait....");
             dialog.setCanceledOnTouchOutside(false);
             dialog.show();
         }
@@ -41,35 +53,37 @@ public class GeoCodeActivity extends AppCompatActivity {
         protected String doInBackground(String... strings) {
             String response;
             try{
-                String address  = strings[0];
+                String address = strings[0];
                 HttpDataHandler http = new HttpDataHandler();
-                String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?address=%s", address);
+                String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?address=%s",address);
                 response = http.getHTTPData(url);
                 return response;
-            } catch(Exception ex){
+            }
+            catch (Exception ex)
+            {
 
             }
             return null;
         }
 
         @Override
-        protected void onPostExecute(String s){
-           try{
-               JSONObject jsonObject = new JSONObject(s);
+        protected void onPostExecute(String s) {
+            try{
+                JSONObject jsonObject = new JSONObject(s);
 
-               String lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
-                       .getJSONObject("location").get("lat").toString();
-               String lng = ((JSONArray)jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
-                       .getJSONObject("location").get("lng").toString();
+                String lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+                        .getJSONObject("location").get("lat").toString();
+                String lng = ((JSONArray)jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+                        .getJSONObject("location").get("lng").toString();
 
-               txtCoord.setText(String.format("Coordinates : %s / %s ",lat,lng));
+                txtCoord.setText(String.format("Coordinates : %s / %s ",lat,lng));
 
-               if(dialog.isShowing())
-                   dialog.dismiss();
+                if(dialog.isShowing())
+                    dialog.dismiss();
 
-           } catch(JSONException e){
-               e.printStackTrace();
-           }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
